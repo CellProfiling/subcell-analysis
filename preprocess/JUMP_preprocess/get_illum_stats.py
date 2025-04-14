70532c6ff9ffc9f1a9f171a9607cf02a353cec39
zwefers <59543549+zwefers@users.noreply.github.com>
2025-04-14 08:34:38 -0700

    Add files via upload

---
+++ preprocess/JUMP_preprocess/get_illum_stats.py
@@ -0,0 +1,28 @@
+import click
+import pandas as pd
+import os
+from preprocessing.illumination_statistics import calculate_statistics
+import multiprocessing
+
+
+
+@click.command()
+@click.option('--metadata_csv', required=True, type=click.Path(exists=True))
+@click.option('--outdir', required=True)
+def get_illum_stats(metadata_csv, outdir):
+    
+    #Make separate DataFrames for plates
+    metadata = pd.read_csv(metadata_csv)
+    plates = metadata["Plate"].unique()
+    plate_dfs = [metadata[metadata["Plate"] == plate].reset_index(drop=True) for plate in plates]
+    outfiles = [f"{outdir}/{plate}_illumstats.pkl" for plate in plates]
+    params = zip(plate_dfs, plates, outfiles)
+
+
+    print(f"Num of CPUS: {multiprocessing.cpu_count()}")
+    os.makedirs(outdir, exist_ok=True)
+    with multiprocessing.Pool() as p:
+        p.starmap(calculate_statistics, params)
+    
+if __name__ == '__main__':
+    get_illum_stats()
---
+++ preprocess/JUMP_preprocess/make_crops.py
@@ -0,0 +1,49 @@
+import click
+from preprocessing.preprocess import preprocess
+from functools import partial
+import pandas as pd
+import multiprocessing
+import os
+
+
+def preprocess_plate(metadata, csv_name, crop_size, save_dir, save_crops=True, center_type="cell", reduce=True):
+    df = preprocess(metadata, crop_size, save_dir, save_crops=True, center_type="cell", reduce=reduce)
+    df.to_csv(csv_name)
+    return df
+
+@click.command()
+@click.option('--metadata_csv', required=True, type=click.Path(exists=True))
+@click.option('--csv_dir', required=True, type=str)
+@click.option('--crop_size', required=True, type=int)
+@click.option('--save_dir', required=True)
+@click.option('--save_crops', default=False, is_flag=True)
+@click.option('--reduce', default=False, is_flag=True)
+def make_crops(metadata_csv, csv_dir, crop_size, save_dir, save_crops, reduce):
+    
+    assert crop_size %2 == 0
+    os.makedirs(save_dir, exist_ok=True)
+    os.makedirs(csv_dir, exist_ok=True)
+
+    #preprocess(metadata_csv, crop_size, save_dir, save_crops=save_crops, reduce=reduce)
+
+    #Make separate DataFrames for plates
+    metadata = pd.read_csv(metadata_csv)
+    #metadata = pd.read_csv(metadata_csv).sample(10).reset_index()
+    plates = metadata["Plate"].unique()
+    plate_dfs = [metadata[metadata["Plate"] == plate].reset_index(drop=True) for plate in plates]
+    csv_names = [f"{csv_dir}/{plate}_centers_test.csv" for plate in plates]
+    params = zip(plate_dfs, csv_names)
+
+    #Partially evaluate preprocess function with fixed arguments
+    f = partial(preprocess_plate, crop_size=crop_size, save_dir=save_dir, save_crops=save_crops, reduce=reduce)
+    #plate_dfs = [(df,) for df in plate_dfs] #iterable format for starmap
+
+    print(f"Num of CPUS: {multiprocessing.cpu_count()}")
+    with multiprocessing.Pool() as p:
+        #dfs = p.map(f, plate_dfs)
+        p.starmap(f, params)
+
+    #pd.concat(dfs).to_csv(csv_name, index=False)
+
+if __name__ == '__main__':
+    make_crops()
